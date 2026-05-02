7cd9e5e26c4cbb6b3b1dbcd91ef606cc48597109
johannesluergen <johannesluergen@gmx.de>
2026-05-02 05:37:45 +0200

    Added basic login window/component. Not sending anything to backend though
    So we have the login component now that gets routed when Angular starts the app.
    You can enter a username plus password. The frontend does very light validation (you cant
    enter empty strings) and displays a Seinfeld-inspired error message.
    
    I still need to add..
    1) actually sending login data to server
    2) add "register for Newbies" option and provide routing
    3) add "Password forgotten?" option and provide routing

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+    userName: string | null = null;
+    email: string | null = null;
+    password: string | null = null;
+    readonly errorMessage: string = "Bad Username/password combination;\nNo learning for you!";
+    badCredentialsFlag = signal<boolean>(false);
+    
+    isUserNameValid(){
+        return !!this.userName; // this will also catch empty strings
+    }
+    isEmailValid(){
+        return (!!this.email) && this.email.includes("@");
+    }
+    isPasswordValid(){
+        return !!this.password;
+    }
+    
+    submitCredentials(){
+        if (!(this.isUserNameValid() && this.isPasswordValid())){
+            this.badCredentialsFlag.set(true);
+        }else{
+            this.badCredentialsFlag.set(false);
+            // send to server, implement later
+            
+        }
+    }
+}
